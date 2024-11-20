d356dd988653aa016a7f50a116c678ea137a8217
talmaikel <tal.mikel@post.runi.ac.il>
2024-11-20 16:09:27 +0200

    complete 3

--- Cheers.java
+++ Cheers.java
@@ -8,9 +8,9 @@ public class Cheers {
                char ch = Character.toUpperCase(name.charAt(i));
 
                 if (ch =='A' ||ch == 'E'||ch == 'F'||ch == 'H'||ch == 'I'||ch == 'L'||ch == 'M'||ch == 'N'||ch == 'O'|| ch == 'R'||ch == 'S'||ch == 'X') {
-                    System.out.println("Give me an "+ch+": "+ch+"!");
+                        System.out.println("Give me an " + ch + ": " + ch + "!");
                 } else {
-                        System.out.println("Give me a "+ch+": "+ch+"!");  
+                        System.out.println("Give me a  " + ch + ": " + ch + "!"); 
                 }
                 
             }
